b671c2f1fe01a14b8ec828fd9aee3deef4cc4c10
Marko Geuseppe Piñeros Crespo <markogeuseppepineroscrespo@wifi-64-4-250.wifi.univ-paris3.fr>
2025-10-08 18:26:27 +0200

    Scripts Bash Exercice 1 Un premier script

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,13 +1,15 @@
-#!user/bin/bash
+
+echo "argument donné : $1"
+
+CHEMIN=$1
 
 echo "Nombre de lieux en 2016" > output.txt
-cat 2016/* | grep Location | wc -l >> output.txt
+cat "$CHEMIN/2016/"*.ann | grep Location | wc -l >> output.txt
 
 echo "Nombre de lieux en 2017" >> output.txt
-cat 2017/* | grep Location | wc -l >> output.txt
+cat "$CHEMIN/2017/"*.ann | grep Location | wc -l >> output.txt
 
 echo "Nombre de lieux en 2018" >> output.txt
-cat 2018/* | grep Location | wc -l >> output.txt
+cat "$CHEMIN/2018/"*.ann | grep Location | wc -l >> output.txt
 
 cat output.txt
-
