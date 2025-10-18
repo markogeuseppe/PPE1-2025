8795d68afa1ebfacac54839ec3e067cbe4c2720c
Marko Geuseppe Piñeros Crespo <markogeuseppepineroscrespo@Host-002.lan>
2025-10-18 21:02:03 +0200

    Scripts Bash Exercice 4 Boucles

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -3,24 +3,20 @@
 if [ $# -ne 1 ]
 then
     echo "Nombre d'arguments incorrect, il en faut 1"
+    echo "Usage: $0 <CHEMIN>"
     exit
 fi
 
-echo "argument donné : $1"
-
 CHEMIN=$1
 
-if [ ! -d $CHEMIN ]
+if [ ! -d "$CHEMIN" ]
 then
     echo "$CHEMIN n'est pas un dossier"
     exit
 fi
 
-echo "Nombre de lieux en 2016 :"
-cat "$CHEMIN/2016/"*.ann | grep Location | wc -l
-
-echo "Nombre de lieux en 2017 :"
-cat "$CHEMIN/2017/"*.ann | grep Location | wc -l
-
-echo "Nombre de lieux en 2018 :"
-cat "$CHEMIN/2018/"*.ann | grep Location | wc -l
+for ANNEE in 2016 2017 2018
+do
+    echo "Nombre de lieux en $ANNEE :"
+    cat "$CHEMIN/$ANNEE/"*.ann | grep Location | wc -l
+done
