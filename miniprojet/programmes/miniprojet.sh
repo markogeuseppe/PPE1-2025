4a162b725b443913705ec70a2c10482cd4588f7a
Marko Geuseppe Piñeros Crespo <markogeuseppepineroscrespo@Host-003.lan>
2025-11-08 22:31:55 +0100

    Exercice miniprojet-2

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
 if [ $# -ne 1 ]
 then
     echo "Nombre d'arguments incorrect, il en faut 1"
@@ -6,6 +5,8 @@ then
 fi
 urls="$1"
 N=0
+echo "<table>" >> ../tableaux/tableau-fr.html
+echo "<tr><th>N</th><th>URL</th><th>HTTP</th><th>TYPE</th><th>MOTS</th></tr>" >> ../tableaux/tableau-fr.html
 while read -r line;
 do
 	N=$(expr $N + 1)
@@ -13,5 +14,6 @@ do
 	CODE=$(echo "$INFOS" | cut -d' ' -f1 )
 	TYPE=$(echo "$INFOS" | cut -d= -f2 )
 	MOTS=$(lynx -dump -nolist ${line} | wc -w)
-	echo -e "${N}\t${line}\t${CODE}\t${TYPE}\t${MOTS}" >> ../tableaux/tableau-fr.tsv;
+	echo -e "<tr><td>${N}</td>\t<td>${line}</td>\t<td>${CODE}</td>\t<td>${TYPE}</td>\t<td>${MOTS}</td></tr>">> ../tableaux/tableau-fr.html
 done < "$1"
+echo "</table>">> ../tableaux/tableau-fr.html
