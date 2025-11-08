df59c7b8821b7be8fafd55014a33c58bc5819aef
Marko Geuseppe Piñeros Crespo <markogeuseppepineroscrespo@Host-003.lan>
2025-11-08 21:15:46 +0100

    Correction mini-projet-1

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -10,7 +10,8 @@ while read -r line;
 do
 	N=$(expr $N + 1)
 	INFOS=$(curl ${line} -s -L -w "%{http_code} %{content_type}" -o /dev/null)
-	HTTP=$(echo "$INFOS" | cut -d= -f2 )
+	CODE=$(echo "$INFOS" | cut -d' ' -f1 )
+	TYPE=$(echo "$INFOS" | cut -d= -f2 )
 	MOTS=$(lynx -dump -nolist ${line} | wc -w)
-	echo -e "${N}\t${line}\t${HTTP}\t${MOTS}" >> ../tableaux/tableau-fr.tsv;
+	echo -e "${N}\t${line}\t${CODE}\t${TYPE}\t${MOTS}" >> ../tableaux/tableau-fr.tsv;
 done < "$1"
