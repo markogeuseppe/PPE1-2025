aa38f416444b4813f777215044c4e53b08d7d797
Marko Geuseppe Piñeros Crespo <markogeuseppepineroscrespo@Host-003.lan>
2025-11-16 22:23:10 +0100

    Script pour git page

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,20 +1,56 @@
+#!/usr/bin/env bash
+
 if [ $# -ne 1 ]
 then
-    echo "Nombre d'arguments incorrect, il en faut 1"
-    exit
+	echo "Le script attend exactement un argument"
+	exit 1
 fi
-urls="$1"
-N=0
 
-echo "<html><head><meta charset=\"UTF-8\"></head><body>
-		<table><tr><th>N</th><th>URL</th><th>HTTP</th><th>TYPE</th><th>MOTS</th></tr>" >> ../tableaux/tableau-fr.html
-while read -r line;
+fichier_urls=$1
+
+echo "<html>
+	<head>
+		<meta charset=\"UTF-8\">
+	</head>
+
+	<body>
+		<table>
+			<tr>
+				<th>numero</th>
+				<th>URL</th>
+				<th>code</th>
+				<th>encodage</th>
+				<th>nombre de mots</th>
+			</tr>"
+
+lineno=1
+while read -r line
 do
-	N=$(expr $N + 1)
-	INFOS=$(curl ${line} -s -L -w "%{http_code} %{content_type}" -o /dev/null)
-	CODE=$(echo "$INFOS" | cut -d' ' -f1 )
-	TYPE=$(echo "$INFOS" | cut -d= -f2 )
-	MOTS=$(lynx -dump -nolist ${line} | wc -w)
-	echo -e "<tr><td>${N}</td>\t<td>${line}</td>\t<td>${CODE}</td>\t<td>${TYPE}</td>\t<td>${MOTS}</td></tr>">> ../tableaux/tableau-fr.html
-done < "$1"
-echo "</table></body></html>">> ../tableaux/tableau-fr.html
+	data=$(curl -s -i -L -w "%{http_code}\n%{content_type}" -o ./.data.tmp $line)
+	http_code=$(echo "$data" | head -1)
+	encoding=$(echo "$data" | tail -1 | grep -Po "charset=\S+" | cut -d"=" -f2)
+
+	if [ -z "${encoding}" ]
+	then
+		encoding="N/A" # petit raccourci qu'on peut utiliser à la place du if : encoding=${encoding:-"N/A"}
+	fi
+
+	nbmots=$(cat ./.data.tmp | lynx -dump -nolist -stdin | wc -w)
+
+	echo -e "			<tr>
+				<td>$lineno</td>
+				<td>$line</td>
+				<td>$http_code</td>
+				<td>$encoding</td>
+				<td>$nbmots</td>
+			</tr>"
+
+	lineno=$(expr $lineno + 1)
+done < $fichier_urls
+
+echo "		</table>
+	</body>
+</html>"
+
+
+rm ./.data.tmp
